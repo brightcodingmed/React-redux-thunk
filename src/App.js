52588d2bba21c00dad575bbcab8bfbdd09a7f0a3
brightcoding <elearndevelopement@gmail.com>
2019-07-05 14:30:20 +0100

    init redux with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import store from './store';
 import './App.css';
 
 function App() {
   return (
+  <Provider store={store}>  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button className="btn btn-primary">salam</button> 
     </div>
+  </Provider>
   );
 }
 
